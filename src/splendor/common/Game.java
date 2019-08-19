3b978887135b23a671fbf3a9768fe97190843380
Logan Gibson <lgibso34@uncc.edu>
2019-08-19 00:34:28 -0400

    player cannot hold more than 10 coins (7 will be implemented when we test with 5 players)
    formatting

--- src/splendor/common/Game.java
+++ src/splendor/common/Game.java
@@ -11,7 +11,7 @@ import splendor.common.coins.CoinPile;
 import splendor.common.util.DeckBuilder;
 import splendor.common.util.Constants;
 
-public class Game{
+public class Game {
 
     private static int index = 0;
     private static int numPlayers = 5;
@@ -24,54 +24,54 @@ public class Game{
     private static CardRow[] cardRows = new CardRow[4];
     private static Hand[] hands = new Hand[0];
 
-    private static void initializeGame(int numPlayers){
+    private static void initializeGame(int numPlayers) {
 
         decks = DeckBuilder.buildDecks();
 
-        for(int i=0; i<Constants.colors.length; i++){
+        for (int i = 0; i < Constants.colors.length; i++) {
             coinPiles[i] = new CoinPile(numPlayers, Constants.colors[i]);
         }
 
-        for(int i=0; i<decks.length; i++){
+        for (int i = 0; i < decks.length; i++) {
             int dealt = 4;
-            if(i == 0){
+            if (i == 0) {
                 dealt = numPlayers + 1;
             }
             cardRows[i] = new CardRow(i, decks[i], dealt);
         }
 
         hands = new Hand[numPlayers];
-        for (int i=0; i<numPlayers; i++){
+        for (int i = 0; i < numPlayers; i++) {
             hands[i] = new Hand();
         }
     }
 
-    public static void addCoinsToPiles(int[] cardCost){
-        for(int i=0; i<cardCost.length; i++){
-            if(cardCost[i] > 0){
-                for (int j=0; j<cardCost[i]; j++){
+    public static void addCoinsToPiles(int[] cardCost) {
+        for (int i = 0; i < cardCost.length; i++) {
+            if (cardCost[i] > 0) {
+                for (int j = 0; j < cardCost[i]; j++) {
                     coinPiles[i].add();
                 }
             }
         }
     }
 
-    public static int removeCoin(int color){
+    public static int removeCoin(int color) {
         return coinPiles[color].remove();
     }
 
-    public static void showDecks(){
-        for(int i=0; i<decks.length; i++){
+    public static void showDecks() {
+        for (int i = 0; i < decks.length; i++) {
             System.out.println("========== Deck " + i + " ==========");
             System.out.println(decks[i].toString());
         }
     }
 
-    public static void showCoinPiles(){
+    public static void showCoinPiles() {
         // show coin piles
         int counter = 1;
         for (CoinPile pile : coinPiles) {
-            if(counter == 6){
+            if (counter == 6) {
                 System.out.println(pile.toString());
                 break;
             }
@@ -80,34 +80,34 @@ public class Game{
         System.out.println();
     }
 
-    public static void showCardRows(){
+    public static void showCardRows() {
         // show card row
         System.out.println(cardRows[0].toString());
-        for(int i=3; i>0; i--){
+        for (int i = 3; i > 0; i--) {
             System.out.println(cardRows[i].toString());
         }
         System.out.println();
     }
 
-    public static void showNumberOfPlayers(){
+    public static void showNumberOfPlayers() {
         System.out.println("Number of Players: " + numPlayers);
         System.out.println();
     }
 
-    public static void showHands(){
-        for(int i=0; i<numPlayers; i++){
-            System.out.println("Hand " + (i+1) + ": " + hands[i].toString());
+    public static void showHands() {
+        for (int i = 0; i < numPlayers; i++) {
+            System.out.println("Hand " + (i + 1) + ": " + hands[i].toString());
         }
     }
 
     //UNTESTED
     // TODO
-    private static Hand handleWinner(){
+    private static Hand handleWinner() {
         Hand winner = hands[playerWhoInitiatedLastRound];
         ArrayList<Hand> ties = new ArrayList<>();
         ties.add(winner);
-        for(Hand player : hands){
-            if(player.getPoints() == winner.getPoints()){
+        for (Hand player : hands) {
+            if (player.getPoints() == winner.getPoints()) {
                 ties.add(player);
             }
             winner = player.getPoints() > winner.getPoints() ? player : winner;
@@ -118,16 +118,16 @@ public class Game{
 
     //UNTESTED
     // TODO
-    private static Hand handleTie(ArrayList<Hand> tiedHands){
+    private static Hand handleTie(ArrayList<Hand> tiedHands) {
         Hand winner = tiedHands.get(0); // start with the first hand
-        for(Hand hand : tiedHands){
+        for (Hand hand : tiedHands) {
             // check if the tiedHands have less total permanent cards than the current 'winner'
             winner = hand.totalPermanentCards() < winner.totalPermanentCards() ? hand : winner;
         }
         return winner;
     }
 
-    public static int handlePlayChoice(Scanner scanner, int player, int choice, int exitDo){
+    public static int handlePlayChoice(Scanner scanner, int player, int choice, int exitDo) {
         int row = 0;
         int cardSpot = 0;
         switch (choice) {
@@ -135,47 +135,61 @@ public class Game{
                 exitDo = 1;
                 break;
             case 1:
+                int playerCoinCount = hands[player].getCoinCount();
                 showCoinPiles();
                 // pick up coins
+                System.out.println("Total coin count: " + playerCoinCount);
                 System.out.println("0: Go back");
                 System.out.println("--------------------------------------------------------------");
                 System.out.println("Select coin color\nOnly enter colors on new lines\n");
 
                 int coinChoice = scanner.nextInt() - 1; // not using try catch to verify that it is an integer since this is only debug mode,
-                if(coinChoice == -1){
+                if (coinChoice == -1) {
                     exitDo = -2;
                     break;
                 }
                 int secondChoice = scanner.nextInt() - 1;
-                if(secondChoice == -1){
+                if (secondChoice == -1) {
                     exitDo = -2;
                     break;
                 }
 
 
                 if (coinChoice != secondChoice) {
-                    coinPiles[coinChoice].remove();
-                    hands[player].addCoin(coinChoice);
-
-                    coinPiles[secondChoice].remove();
-                    hands[player].addCoin(secondChoice);
-
-                    int thirdChoice = scanner.nextInt();
-                    coinPiles[thirdChoice].remove();
-                    hands[player].addCoin(thirdChoice);
-                } else {
-                    if(coinPiles[coinChoice].getSize() >= 4) {
+                    if (playerCoinCount < 8) {
                         coinPiles[coinChoice].remove();
                         hands[player].addCoin(coinChoice);
 
                         coinPiles[secondChoice].remove();
                         hands[player].addCoin(secondChoice);
-                    }else{
-                        System.out.println("There must be four coins available for you to grab two.");
+
+                        int thirdChoice = scanner.nextInt() - 1;
+                        coinPiles[thirdChoice].remove();
+                        hands[player].addCoin(thirdChoice);
+
+                    } else {
+                        // this will contain a bug until this is cleaned up
+                        // for instance if the user can only grab two coins of different colors
+                        System.out.println("You would go over 10 coins.");
+                        exitDo = -2;
+                        break;
+                    }
+
+                } else if (coinPiles[coinChoice].getSize() >= 4) {
+                    if (playerCoinCount < 9) {
+                        coinPiles[coinChoice].remove();
+                        hands[player].addCoin(coinChoice);
+
+                        coinPiles[secondChoice].remove();
+                        hands[player].addCoin(secondChoice);
+                    } else {
+                        System.out.println("There must be four coins available for you to grab two or you would go over 10 coins.");
                         exitDo = -2;
                         break;
                     }
                 }
+
+
                 exitDo = 0;
                 break;
             case 2:
@@ -188,44 +202,44 @@ public class Game{
                     exitDo = -2;
                     break;
                 } else {
-                System.out.println("Choose card spot (1-4): ");
-                cardSpot = scanner.nextInt() - 1;
+                    System.out.println("Choose card spot (1-4): ");
+                    cardSpot = scanner.nextInt() - 1;
 
-                int playerCanBuy = hands[player].checkBalance(cardRows[row].peekCard(cardSpot));
-                int playerWillUseGoldCoin = 0;
+                    int playerCanBuy = hands[player].checkBalance(cardRows[row].peekCard(cardSpot));
+                    int playerWillUseGoldCoin = 0;
 
-                if (playerCanBuy >= 0) {
-                    if(playerCanBuy > 0){
-                        System.out.println("You must use " + playerCanBuy +  " gold coin(s) to purchase this card. Proceed?\n0 for no\n1 for yes");
-                        playerWillUseGoldCoin = scanner.nextInt();
-                        if(playerWillUseGoldCoin == 0){
-                            exitDo = -2;
-                            break;
+                    if (playerCanBuy >= 0) {
+                        if (playerCanBuy > 0) {
+                            System.out.println("You must use " + playerCanBuy + " gold coin(s) to purchase this card. Proceed?\n0 for no\n1 for yes");
+                            playerWillUseGoldCoin = scanner.nextInt();
+                            if (playerWillUseGoldCoin == 0) {
+                                exitDo = -2;
+                                break;
+                            }
                         }
+
+                        int[] cardCost = hands[player].getCost(cardRows[row].peekCard(cardSpot));
+                        if (playerWillUseGoldCoin == 1) {
+                            int[] oldCardCost = new int[cardCost.length + 1];
+                            for (int i = 0; i < cardCost.length; i++) {
+                                oldCardCost[i] = cardCost[i];
+                            }
+                            oldCardCost[5] = playerCanBuy;
+
+                            cardCost = new int[cardCost.length + 1];
+                            for (int i = 0; i < cardCost.length; i++) {
+                                cardCost[i] = oldCardCost[i];
+                            }
+                        }
+                        Card pickedUpCard = cardRows[row].removeAndReplace(cardSpot, decks[row].dealCard());
+                        addCoinsToPiles(cardCost);
+                        hands[player].addCard(pickedUpCard, cardCost);
+                        exitDo = 0;
+                    } else {
+                        System.out.println("You do not have the balance for this card.");
+                        exitDo = -2;
                     }
-
-                    int[] cardCost = hands[player].getCost(cardRows[row].peekCard(cardSpot));
-                    if(playerWillUseGoldCoin == 1){
-                        int[] oldCardCost = new int[cardCost.length + 1];
-                        for(int i=0; i<cardCost.length; i++){
-                            oldCardCost[i] = cardCost[i];
-                        }
-                        oldCardCost[5] = playerCanBuy;
-
-                        cardCost = new int[cardCost.length + 1];
-                        for(int i=0; i<cardCost.length; i++){
-                            cardCost[i] = oldCardCost[i];
-                        }
-                    }
-                    Card pickedUpCard = cardRows[row].removeAndReplace(cardSpot, decks[row].dealCard());
-                    addCoinsToPiles(cardCost);
-                    hands[player].addCard(pickedUpCard, cardCost);
-                    exitDo = 0;
-                } else {
-                    System.out.println("You do not have the balance for this card.");
-                    exitDo = -2;
-                }
-                break;
+                    break;
                 }
             case 3:
                 // show hands and card rows
@@ -253,7 +267,7 @@ public class Game{
                         hands[player].reserveCard(pickedUpCard);
                         hands[player].addCoin(removeCoin(5)); // add a gold coin while removing it from this game pile
                         exitDo = 0;
-                    }else{
+                    } else {
                         System.out.println("You already have 3 reserved cards.");
                         exitDo = -2;
                     }
@@ -274,25 +288,25 @@ public class Game{
                     int playerWillUseGoldCoin = 0;
 
                     if (playerCanBuy >= 0) {
-                        if(playerCanBuy > 0){
-                            System.out.println("You must use " + playerCanBuy +  " gold coin(s) to purchase this card. Proceed?\n0 for no\n1 for yes");
+                        if (playerCanBuy > 0) {
+                            System.out.println("You must use " + playerCanBuy + " gold coin(s) to purchase this card. Proceed?\n0 for no\n1 for yes");
                             playerWillUseGoldCoin = scanner.nextInt();
-                            if(playerWillUseGoldCoin == 0){
+                            if (playerWillUseGoldCoin == 0) {
                                 exitDo = -2;
                                 break;
                             }
                         }
                         int[] cardCost = hands[player].getCost(hands[player].peekCard(row));
 
-                        if(playerWillUseGoldCoin == 1){
+                        if (playerWillUseGoldCoin == 1) {
                             int[] oldCardCost = new int[cardCost.length + 1];
-                            for(int i=0; i<cardCost.length; i++){
+                            for (int i = 0; i < cardCost.length; i++) {
                                 oldCardCost[i] = cardCost[i];
                             }
                             oldCardCost[5] = playerCanBuy;
 
                             cardCost = new int[cardCost.length + 1];
-                            for(int i=0; i<cardCost.length; i++){
+                            for (int i = 0; i < cardCost.length; i++) {
                                 cardCost[i] = oldCardCost[i];
                             }
                         }
@@ -300,7 +314,7 @@ public class Game{
                         addCoinsToPiles(cardCost);
                         exitDo = 0;
                         break;
-                    }else{
+                    } else {
                         System.out.println("you do not have the balance for that card");
                     }
                 }
@@ -314,7 +328,7 @@ public class Game{
     public static void main(String[] args) {
         boolean play = false;
 
-        if(debug) {
+        if (debug) {
             Scanner scanner = new Scanner(System.in);
 
             int exitDo = 0;
@@ -323,25 +337,25 @@ public class Game{
                 try {
                     System.out.print("Enter number of players: ");
                     temp = scanner.nextInt();
-                    if(temp > 1 && temp < 6){
+                    if (temp > 1 && temp < 6) {
                         numPlayers = temp;
                         exitDo = 1; // exit do loop
-                    }else{
+                    } else {
                         System.out.println("Must be between 2-5 players");
                     }
-                }catch (InputMismatchException e){
+                } catch (InputMismatchException e) {
                     System.out.println("Must enter an integer");
                     scanner.next();
                 }
 
-            }while (exitDo == 0);
+            } while (exitDo == 0);
 
             initializeGame(numPlayers);
 
             exitDo = 0;
             int choice = 0;
             do {
-                try{
+                try {
                     System.out.println("--------------------------------------------------------------");
                     System.out.println("0: Exit\n" +
                             "1. Show the number of Players\n" +
@@ -354,9 +368,9 @@ public class Game{
 
                     temp = scanner.nextInt();
                     System.out.println();
-                    if(temp >= 0 && temp <= 6){
+                    if (temp >= 0 && temp <= 6) {
                         choice = temp;
-                        switch (choice){
+                        switch (choice) {
                             case 0:
                                 exitDo = 1;
                                 break;
@@ -381,24 +395,24 @@ public class Game{
                             default:
                                 break;
                         }
-                    }else{
+                    } else {
                         System.out.println("Choose 0-6");
                     }
-                }catch (InputMismatchException e){
+                } catch (InputMismatchException e) {
                     System.out.println("Must enter an integer");
                     scanner.next();
                 }
-            }while (exitDo == 0);
+            } while (exitDo == 0);
 
             // if the programmer chooses to play ----------------------------------------------------------------------------------------------------------------------------------------------------------
-            if(play){
+            if (play) {
                 int player = 0;
                 exitDo = 0;
                 choice = 0;
                 do {
-                    try{
+                    try {
                         System.out.println("--------------------------------------------------------------");
-                        System.out.println("Player " + (player+1));
+                        System.out.println("Player " + (player + 1));
                         System.out.println("0: Exit\n" +
                                 "1. Pickup coins\n" +
                                 "2. Buy a card\n" +
@@ -409,14 +423,14 @@ public class Game{
 
                         temp = scanner.nextInt();
                         System.out.println();
-                        if(temp >= 0 && temp <= 5){
+                        if (temp >= 0 && temp <= 5) {
                             choice = temp;
                             exitDo = -1;
-                            while(exitDo == -1){
+                            while (exitDo == -1) {
                                 exitDo = handlePlayChoice(scanner, player, choice, exitDo);
                             }
 
-                            if(exitDo != -2) {
+                            if (exitDo != -2) {
                                 // untested
                                 // TODO
                                 if (lastRound) {
@@ -437,18 +451,18 @@ public class Game{
                                 }
                             }
 
-                        }else{
+                        } else {
                             System.out.println("Choose 0-5");
                         }
-                    }catch (InputMismatchException e){
+                    } catch (InputMismatchException e) {
                         System.out.println("Must enter an integer");
                         scanner.next();
                     }
-                }while (exitDo <= 0);
+                } while (exitDo <= 0);
 
                 handleWinner();
             }
-        }else{
+        } else {
             initializeGame(numPlayers);
             showNumberOfPlayers();
             showDecks();
