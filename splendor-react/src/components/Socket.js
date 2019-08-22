8c9826f82034dcb3cbe47d12f09d4c335b8f4506
sherrilln <nicholas.sherrill@leidos.com>
2019-08-22 13:34:51 -0400

    told client to connect to server
    There's some kind of protocol error preventing messages from going through tho

---
+++ splendor-react/src/components/Socket.js
@@ -0,0 +1,26 @@
+const DefaultConfig = {
+    PROTOCOL: 'wss://',
+    HOST: 'localhost',
+    PORT: ':59001'
+}
+
+const SingleSocket = (function () {
+    let instance;
+
+
+    function createInstance(specIP) {
+        const socket = new WebSocket(DefaultConfig.PROTOCOL + (specIP ? specIP : DefaultConfig.HOST) + DefaultConfig.PORT)
+        return socket
+    }
+
+    return {
+        getInstance: function(specIP) {
+            if(!instance){
+                instance = createInstance(specIP)
+            }
+            return instance
+        }
+    };
+})();
+
+export default SingleSocket
